c23fad89ad3041c3ea29e59e61eda1d04442bc2d
dorntrevor7 <dorntrevor7@gmail.com>
2022-12-06 16:23:52 -0700

    browser routes with header setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+
+const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>{" "}
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
